1ff0c31dcd06b53c2a1b2cb6e97885d6e324e5fa
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-07-23 13:19:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -181,7 +181,7 @@ sleep 2
 # --- 3. PACKAGES  ---
 log_info "Instalasi Packages Sparow OS"
 
-pacstrap -K /mnt base base-devel wireless-regdb linux-lts linux-lts-headers $firms $ucodes \
+pacstrap -K /mnt base base-devel wireless-regdb linux $firms $ucodes \
     networkmanager plasma-nm firewalld git wget neovim \
     efibootmgr grub os-prober iptables-nft dolphin xorg-server \
     pipewire pipewire-alsa pipewire-jack pipewire-pulse mkinitcpio \
