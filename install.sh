e7ce3df135642a9cce37cc01498300d28b428ef0
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-07-24 13:41:55 +0700

    update

--- install.sh
+++ install.sh
@@ -189,7 +189,6 @@ pacstrap /mnt base base-devel wireless-regdb linux $firms $ucodes \
 
 # Configuration
 rm -fr /mnt/usr/share/wallpapers/Next/contents/images/* &&
-rm -fr /mnt/usr/share/plasma/look-and-feel/* &&
 git clone https://github.com/linux-sparow/installer-new.git installer &&
 cp -fr installer/config/* /mnt/ &&
 rm -fr installer &&
